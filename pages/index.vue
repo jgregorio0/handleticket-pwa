68b39ec537fa39218468dad4b557e571d290862a
jgregorio <gregoriojesus0@gmail.com>
2021-04-26 00:20:33 +0200

    Add home page

--- pages/index.vue
+++ pages/index.vue
@@ -1,7 +1,34 @@
 <template>
-  <section class="container">Hola</section>
+  <section class="container">
+    <div class="row">
+      <div class="col-md-4 col-sm-12">
+        <h1>Transforma tickets a una hoja de calculo</h1>
+        <b-button variant="success" @click="goToTicket()">Prueba aqui</b-button>
+        <p>Extrae informacion a traves de tus tickets de compra.</p>
+        <ul>
+          <li>1. Haz una foto al ticket</li>
+          <li>2. Extrae su contenido</li>
+          <li>3. Transforma el contenido para visualizarlo mejor</li>
+          <li>4. Imprime tu hoja de calculo</li>
+        </ul>
+      </div>
+      <div class="col-md-8 col-sm-12">
+        <b-img
+          src="/bills-mobile.jpg"
+          fluid
+          alt="Handle Tickets Mobile"
+        ></b-img>
+      </div>
+    </div>
+  </section>
 </template>
 
 <script>
-export default {}
+export default {
+  methods: {
+    goToTicket() {
+      this.$router.push('/ticket')
+    },
+  },
+}
 </script>
